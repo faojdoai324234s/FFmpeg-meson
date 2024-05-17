a9596994af5fff964ced059b7401ac9a5efc6dc1
Kacper Michajłow <kasper93@gmail.com>
2024-05-17 20:06:55 +0200

    meson: Add support for negated checks
    This fixes parsing of syntax like `OBJS-$(!CONFIG_VULKAN)`,
    which adds objects only if a certain feature is disabled.
    
    Fixes: #44
    Part-of: <https://gitlab.freedesktop.org/gstreamer/meson-ports/ffmpeg/-/merge_requests/116>
    (cherry picked from commit 7627ec4a7f6cd70b8e804e2e042d222322501b85)

--- parse_sources.py
+++ parse_sources.py
@@ -87,9 +87,11 @@ def make_to_meson(path):
 
             if accumulate:
                 ofiles = l
-            elif re.match('OBJS-.*CONFIG.*\+\=.*', l):
-                label, ofiles = l.split('+=')
-                label = label.split('CONFIG_')[1].rstrip(' )')
+            elif m := re.match('OBJS-\$\((?P<neg>!?)(?:CONFIG|HAVE)_(?P<label>[^\)]+)\)\s*\+=\s*(?P<files>.+)', l):
+                label = m.group('label')
+                if m.group('neg'):
+                    label = '!' + label
+                ofiles = m.group('files')
                 source_type = 'c' # arguable ^^
             elif re.match('OBJS-.*HAVE.*\+\=.*', l):
                 label, ofiles = l.split('+=')
