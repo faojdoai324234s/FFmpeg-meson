1c63dc62c961ec5d04de11f86e79321dd8efea24
Frank Richter <frank.richter@gmail.com>
2024-11-14 15:58:29 +0100

    Simplify rc_workaround.py
    * Per https://gitlab.freedesktop.org/gstreamer/meson-ports/ffmpeg/-/merge_requests/93#note_2015643,
    the first directory RC searches for includes is the current directory.
    * rc_workaround.py places the patched .rc file in the build dir.
    * The desired config.h is in the build dir.
    * Ergo, there's actually no patching necessary, simply copying the file already
      makes it use the right config.h.
    
    The patching actually broke building when using build directories nested
    >= two levels deep. Replacing the patch with a copy fixes this issue.
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/meson-ports/ffmpeg/-/merge_requests/132>

--- rc_workaround.py
+++ rc_workaround.py
@@ -5,28 +5,13 @@
 
 from argparse import ArgumentParser
 from pathlib import Path
-import os
-import re
 
 if __name__ == '__main__':
-    parser = ArgumentParser(description='Patch .rc file to work around RC include precedences')
+    parser = ArgumentParser(description='Copy .rc file to work around RC include precedences')
     parser.add_argument('input', type=Path, help='Input file to patch')
-    parser.add_argument('build_folder', type=Path, help='Build folder')
 
     args = parser.parse_args()
 
-    base_folder: Path = args.input.parent
-
-    build_folder: Path = args.build_folder
-
-    common_root = os.path.relpath(build_folder, base_folder)
-
-    relative_path = Path(common_root).as_posix()
-
     with args.input.open('r', encoding='utf-8') as f:
         for line in f.readlines():
-            if re.match('^#include "config.h"', line):
-                # use <folder of the input>/../.../config.h
-                print(f'#include "{relative_path}/config.h"')
-            else:
-                print(line.strip())
+            print(line.rstrip())
