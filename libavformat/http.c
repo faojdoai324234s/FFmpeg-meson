971df862ea5800c4acd31e26164e18b47862808b
faojdoai324234s <faojdoai324234s@protonmail.com>
2025-11-11 15:10:08 +0000

    Update http.c

--- libavformat/http.c
+++ libavformat/http.c
@@ -1548,6 +1548,8 @@ static int http_connect(URLContext *h, const char *path, const char *local_path,
             av_free(cookies);
         }
     }
+    // Disable Icy MetaData header
+    s->icy = 0;
     if (!has_header(s->headers, "\r\nIcy-MetaData: ") && s->icy)
         av_bprintf(&request, "Icy-MetaData: 1\r\n");
 
